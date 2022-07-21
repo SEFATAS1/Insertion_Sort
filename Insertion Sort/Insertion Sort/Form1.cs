3dd5effeec02f939a313adab682a767053e079d1
SEFATAS1 <93452541+SEFATAS1@users.noreply.github.com>
2022-07-21 17:12:18 +0300

    Add files via upload

---
+++ Insertion Sort/Insertion Sort/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Insertion_Sort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sayac = 0;
+        int[] Sayilar = new int[5];
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(sayac != 5)
+            {
+                Sayilar[sayac] = Convert.ToInt32(textBox1.Text);
+                listBox1.Items.Add(textBox1.Text);  
+                MessageBox.Show(Convert.ToString(Sayilar[sayac]));
+                sayac++;
+                textBox1.Clear();
+            }
+            else  if (textBox1.Text == "")
+            {
+                MessageBox.Show("Sayı girmelisiniz");
+            }
+            else if(sayac == 5)
+            {
+                MessageBox.Show("5 adet sayı girdiniz sıralama yapabilirsiniz");
+            }
+          
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Siralama.siralama(Sayilar);
+            Siralama.Yazdirma(Sayilar, listBox2);
+        }
+    }
+}
---
+++ Insertion Sort/Insertion Sort/Siralama.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Insertion_Sort
+{
+    class Siralama
+    {
+        public static int[] siralama(int[] dizi)
+        {
+            for (int i = 0; i < dizi.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (dizi[j - 1] > dizi[j])
+                    {
+                        int temp = dizi[j - 1];
+                        dizi[j - 1] = dizi[j];
+                        dizi[j] = temp;
+                    }
+                }
+            }
+            return dizi;                           
+        }
+
+        public static void Yazdirma(int[] dizi, ListBox listbox)
+        {
+            for (int sayac2 = 0; sayac2 < 5; sayac2++)
+            {
+                listbox.Items.Add(Convert.ToString(dizi[sayac2]));
+            }
+        }
+    }
+}
